2f1f108dadff864835e4185eec8393fc2eeefc83
Kaz van Wel <info@kiksaus.nl>
2022-03-30 12:16:11 +0200

    Added login and dark mode toggle component

---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      darkMode: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.darkMode = !this.darkMode;
+    },
+    setDark() {
+      this.darkMode = true;
+    },
+    setLight() {
+      this.darkMode = false;
+    }
+  },
+  watch:{
+    darkMode(val){
+      document.body.classList.toggle('darkmode', val)
+    }
+  },
+  mounted(){
+    this.darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+});
+</script>
+
+<template>
+  <button class="darkmode-toggle" @click="toggle()">
+    Switch to {{ darkMode ? 'light' : 'dark' }}
+  </button>
+</template>
+
+<style scoped>
+.darkmode-toggle {
+  position: absolute;
+  z-index: 1;
+  top: 10px;
+  right: 10px;
+}
+</style>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+// defineProps<{
+//   msg: string;
+// }>();
+</script>
+
+<template>
+  <div class="login">
+    <div class="login-box">
+      <h1 class="green">{{ msg }}</h1>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      Login gool!!! Login gool!!! Login gool!!!<br>Login gool!!! Login gool!!! Login gool!!!<br>
+      JAAAAA
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+  background: url("login-bg.png") no-repeat center center;
+  width: 100%;
+}
+
+.login-box {
+  background-color: var(--color-background);
+  padding: 4rem;
+  border-radius: 2rem;
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+
+body.darkmode .login:before {
+  content: '';
+  background-color: black;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
