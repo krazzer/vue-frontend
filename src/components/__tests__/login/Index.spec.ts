3bcb1587a10e29377ea2ab39b59584969a119ae0
Kaz van Wel <info@kiksaus.nl>
2022-09-12 14:02:10 +0200

    Fixed Login component test

--- src/components/__tests__/login/Index.spec.ts
+++ src/components/__tests__/login/Index.spec.ts
@@ -1,10 +1,11 @@
 import {describe, it, expect} from "vitest";
 import { mount } from "@vue/test-utils";
 import Login from "@/components/login/Index.vue";
+import { plugin, defaultConfig } from '@formkit/vue'
 
 describe("Login", () => {
   it("renders properly", () => {
-    const wrapper = mount(LoginPage, { props: {} });
-    expect(wrapper.text()).toContain("");
+    const wrapper = mount(Login, {global: { plugins: [[plugin, defaultConfig]] }});
+    expect(wrapper.html()).toContain("<form");
   });
 });
