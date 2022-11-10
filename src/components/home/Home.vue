c5cdf23117285e00f8ad1811eaee4553c381d7f4
Kaz van Wel <info@kiksaus.nl>
2022-11-10 09:35:30 +0100

    WIP: Responsive menu

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -18,7 +18,8 @@ export default defineComponent({
       dataTable: {},
       media: {},
       selectedMenuItem: '',
-      html: ''
+      html: '',
+      mobileMenuOpen: false,
     }
   },
   mounted() {
@@ -80,11 +81,14 @@ export default defineComponent({
     /**
      * @param data
      */
-    setContentByResponseData(data: any)
-    {
-      this.html = data.html;
+    setContentByResponseData(data: any) {
+      this.html      = data.html;
       this.dataTable = data.dataTable;
-      this.media = data.media;
+      this.media     = data.media;
+    },
+
+    toggleMenu(){
+      this.mobileMenuOpen = !this.mobileMenuOpen;
     }
   }
 });
@@ -98,7 +102,7 @@ import Media from "@/components/media/Media.vue";
 </script>
 
 <template>
-  <div class="sidebar">
+  <div class="sidebar" :class="{ open: mobileMenuOpen }">
     <div class="sidebar__logo">
       <Logo/>
     </div>
@@ -106,14 +110,21 @@ import Media from "@/components/media/Media.vue";
       <Menu :menu="menu" :selectedItem="selectedMenuItem" :logout="logout"/>
     </div>
   </div>
-  <div class="main">
+  <div class="main" @click="toggleMenu">
+    <div class="menu-button">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
     <span v-if="html" v-html="html"></span>
-    <Media v-if="media" :settings="media" />
-    <DataTable v-if="dataTable" :settings="dataTable" :instance="dataTable.instance" />
+    <Media v-if="media" :settings="media"/>
+    <DataTable v-if="dataTable" :settings="dataTable" :instance="dataTable.instance"/>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@import "@/assets/media-query-sizes.scss";
+
 $sideBarWidth: 250px;
 $mainPadding: 40px;
 
@@ -125,7 +136,7 @@ $mainPadding: 40px;
   height: 100%;
   overflow: auto;
   z-index: 1;
-  transition: background-color var(--color-scheme-transition-speed);
+  transition: background-color var(--color-scheme-transition-speed), margin-left .3s;
 
   &__logo {
     margin-bottom: $mainPadding;
@@ -134,9 +145,87 @@ $mainPadding: 40px;
   &__menu {
     position: relative;
   }
+
+  @media (max-width: $screen-sm-max) {
+    margin-left: -$sideBarWidth;
+
+    &.open{
+      margin-left: 0;
+    }
+
+    .sidebar__logo {
+      display: none;
+    }
+  }
 }
 
 .main {
   padding: $mainPadding $mainPadding $mainPadding $mainPadding + $sideBarWidth;
+
+  @media (max-width: $screen-sm-max) {
+    padding-left: $mainPadding;
+  }
+}
+
+.menu-button {
+  position: relative;
+  z-index: 2;
+  width: 36px;
+  height: 20px;
+  transform: rotate(0deg);
+  transition: .5s ease-in-out;
+  cursor: pointer;
+  margin-bottom: 40px;
+  display: none;
+
+  span {
+    display: block;
+    position: absolute;
+    height: 3px;
+    width: 100%;
+    background: var(--color-text);
+    opacity: 1;
+    left: 0;
+    transform: rotate(0deg);
+    transition: .25s ease-in-out;
+
+    &:nth-child(1) {
+      top: 0;
+      transform-origin: left center;
+    }
+
+    &:nth-child(2) {
+      top: 10px;
+      transform-origin: left center;
+    }
+
+    &:nth-child(3) {
+      top: 20px;
+      transform-origin: left center;
+    }
+  }
+
+  @media (max-width: $screen-sm-max) {
+      display: block;
+  }
+}
+
+body.menu-open .header__nav__mobile-button span{
+  &:nth-child(1) {
+    transform: rotate(45deg);
+    top: -1px;
+    left: 6px;
+  }
+
+  &:nth-child(2) {
+    width: 0;
+    opacity: 0;
+  }
+
+  &:nth-child(3) {
+    transform: rotate(-45deg);
+    top: 27px;
+    left: 6px;
+  }
 }
 </style>
