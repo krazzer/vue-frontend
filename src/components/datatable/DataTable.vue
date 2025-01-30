651ad9b636771193b1bfd082ccdd92d421d546ec
Kaz van Wel <info@kiksaus.nl>
2025-01-30 13:33:38 +0100

    Move DataTable toolbar to its own component

--- src/components/datatable/DataTable.vue
+++ src/components/datatable/DataTable.vue
@@ -5,13 +5,13 @@ import Row from "./subcomponents/Row.vue";
 import Svg from "@/components/svg/Svg.vue";
 import DragAndDropPages from "./classes/dragAndDropPages";
 import ToolbarSearch from "@/components/toolbarsearch/ToolbarSearch.vue";
-import Toolbar from "@/mixins/Toolbar.vue";
 import DragAndDropTable from "@/mixins/DragAndDropTable.vue";
+import DataTableToolbar from "@/components/datatable/subcomponents/DataTableToolbar.vue";
 
 export default defineComponent({
   name: "DataTable",
-  components: {ToolbarSearch, Svg, EditDialog, Row},
-  mixins: [Toolbar, DragAndDropTable],
+  components: {DataTableToolbar, ToolbarSearch, Svg, EditDialog, Row},
+  mixins: [DragAndDropTable],
   props: {
     darkMode: Boolean,
     settings: {
@@ -226,15 +226,6 @@ export default defineComponent({
       return classes;
     },
 
-    /**
-     * @param button
-     */
-    isDisabled(button: any) {
-      if (button.action == 'delete') {
-        return this.selected.length < 1;
-      }
-    },
-
     async init() {
       this.error = '';
 
@@ -443,47 +434,9 @@ export default defineComponent({
       {{ error }}
     </div>
     <template v-else>
-      <div class="datatable__toolbar">
-        <div class="datatable__toolbar__buttons" ref="toolbarButtons" :class="isWrapped ? 'wrapped' : ''">
-          <template v-for="button in buttons">
-            <v-menu v-if="button.type == 'menu'">
-              <template v-slot:activator="{ props }">
-                <v-btn v-bind="props" :prepend-icon="'mdi-menu'">{{ button.label }}</v-btn>
-              </template>
-              <v-list>
-                <v-list-item v-for="(item, index) in button.items" :key="index" :value="index"
-                             @click="buttonClick(item)">
-                  <template v-slot:prepend>
-                    <v-icon :icon="item.icon"></v-icon>
-                  </template>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-            <v-chip v-else-if="button.type == 'label'" :prepend-icon="button.icon">{{ button.label }}</v-chip>
-            <v-btn v-else @click="buttonClick(button)" :disabled="isDisabled(button)" :prepend-icon="button.icon">
-              {{ button.label }}
-            </v-btn>
-          </template>
-
-          <template v-for="filter in filters">
-            <v-select :multiple="filter.multiple" :items="filter.items" v-model="filterValues[filter.key]"
-                      :label="filter.label" density="compact" :max-width="filter.width ? filter.width : 200"
-                      return-object :clearable="!filter.multiple" min-width="135"></v-select>
-          </template>
-          <div class="datatable__toolbar__buttons__right">
-            <div v-if="pages" class="pages">
-            <span class="page" :class="pageNr ? (pageNr == page ? 'selected' : '') : 'disabled'" v-for="pageNr in pages"
-                  @click="setPage(pageNr)">
-              {{ pageNr ? pageNr : '...' }}
-            </span>
-            </div>
-            <v-select v-if="languages" :items="languages" item-title="label" @update:modelValue="changeLanguage"
-                      item-value="key" density="compact" class="language" v-model="language"/>
-            <ToolbarSearch @search="searchAction"/>
-          </div>
-        </div>
-      </div>
+      <DataTableToolbar :buttons="buttons" :filters="filters" :pages="pages" :languages="languages" :language="language"
+               :search="search" @buttonClick="buttonClick" @changeLanguage="changeLanguage" @searchAction="searchAction"
+               @setPage="setPage" :selected="selected" :filterValues="filterValues" :page="page"/>
       <div class="datatable__table">
         <table ref="table">
           <thead>
@@ -510,7 +463,7 @@ export default defineComponent({
         </table>
       </div>
       <div class="bottom-bar">
-        <div v-if="pages" class="pages">
+        <div v-if="pages" class="pagination">
             <span class="page" :class="pageNr ? (pageNr == page ? 'selected' : '') : 'disabled'" v-for="pageNr in pages"
                   @click="setPage(pageNr)">
               {{ pageNr ? pageNr : '...' }}
@@ -525,102 +478,13 @@ export default defineComponent({
 </template>
 
 <style lang="scss" scoped>
+@use '@/assets/css/pagination';
+
 :deep(.v-list-item__prepend) {
   width: 40px;
 }
 
 .datatable {
-
-  &__toolbar {
-    margin-bottom: $spaceLogoMenu;
-
-    &__buttons {
-      $self: &;
-      display: flex;
-      flex-wrap: wrap;
-      gap: 5px;
-
-      :deep(.icon--add) {
-        top: 1px;
-      }
-
-      :deep(.icon--delete) {
-        top: 1px;
-      }
-
-      :deep(svg) {
-        width: 20px;
-        height: 20px;
-        margin-right: 5px;
-        fill: var(--color-text);
-
-        line {
-          stroke: var(--color-text);
-        }
-      }
-
-      &__right {
-        margin-left: auto;
-        display: flex;
-        gap: 5px;
-        white-space: nowrap;
-        flex-wrap: wrap;
-      }
-
-      .v-btn--disabled {
-        :deep(svg) {
-          opacity: .3;
-        }
-      }
-
-      &.wrapped {
-        #{$self}__right {
-          margin-left: 0;
-        }
-      }
-
-      .v-chip {
-        height: 36px;
-        padding: 0 16px;
-      }
-    }
-
-    .v-select {
-      height: 36px;
-
-      :deep(.v-input__details) {
-        position: absolute;
-      }
-
-      :deep(.v-input__control) {
-        height: 36px;
-      }
-
-      :deep(.v-field-label) {
-        margin-top: -1px;
-      }
-
-      :deep(.v-field--focused .v-field-label), &--selected :deep(.v-field-label) {
-        margin-top: -9px;
-        margin-left: 10px;
-        font-size: 10px;
-      }
-
-      :deep(input), :deep(.v-field__input) {
-        padding: 6px 10px;
-        margin-top: -2px;
-      }
-
-      :deep(.v-field__input) {
-        flex-flow: nowrap;
-        overflow: hidden;
-      }
-    }
-
-    .language {
-      max-width: 200px;
-    }
-  }
 
   &__table {
     table {
@@ -688,40 +552,6 @@ export default defineComponent({
     }
   }
 
-  .pages {
-    display: inline-block;
-
-    .page {
-      display: inline-block;
-      padding: 5px;
-      border: 1px solid var(--color-line);
-      min-width: 38px;
-      text-align: center;
-      margin-right: -1px;
-
-      &:not(.disabled):hover {
-        background-color: var(--color-background-shade3);
-        cursor: pointer;
-      }
-
-      &:last-child {
-        border-bottom-right-radius: var(--border-radius);
-        border-top-right-radius: var(--border-radius);
-      }
-
-      &:first-child {
-        border-bottom-left-radius: var(--border-radius);
-        border-top-left-radius: var(--border-radius);
-      }
-
-      &.selected, &.selected:hover {
-        background-color: var(--main-color);
-        border-color: var(--main-color);
-        color: var(--color-text-in-main-bg);
-      }
-    }
-  }
-
   &.noselect {
     @include noSelect();
   }
@@ -743,11 +573,11 @@ export default defineComponent({
     tbody :deep(tr:last-child td:last-child) {
       border-bottom-right-radius: var(--border-radius);
     }
-  }
 
-  .datatable__toolbar {
-    margin-bottom: 20px;
-    padding: 15px;
+    :deep(.toolbar) {
+      margin-bottom: 20px;
+      padding: 15px;
+    }
   }
 }
 
---
+++ src/components/datatable/subcomponents/DataTableToolbar.vue
@@ -0,0 +1,170 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+import ToolbarSearch from "@/components/toolbarsearch/ToolbarSearch.vue";
+import Toolbar from "@/mixins/Toolbar.vue";
+
+export default defineComponent({
+  name: "DataTableToolbar",
+  components: {ToolbarSearch},
+  mixins: [Toolbar],
+  props: ['buttons', 'filters', 'pages', 'languages', 'language', 'search', 'selected', 'filterValues', 'page'],
+  emits: ["buttonClick", "changeLanguage", "searchAction", "setPage"],
+  methods: {
+    buttonClick(button: any) {
+      this.$emit("buttonClick", button);
+    },
+    changeLanguage(val: string) {
+      this.$emit("changeLanguage", val);
+    },
+    searchAction(search: string) {
+      this.$emit("searchAction", search);
+    },
+    setPage(page: number) {
+      this.$emit("setPage", page);
+    },
+    isDisabled(button: any) {
+      if (button.action == 'delete') {
+        return this.selected.length < 1;
+      }
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="toolbar">
+    <div class="toolbar__buttons" :class="isWrapped ? 'wrapped' : ''">
+      <template v-for="button in buttons">
+        <v-menu v-if="button.type == 'menu'">
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" :prepend-icon="'mdi-menu'">{{ button.label }}</v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in button.items" :key="index" :value="index" @click="buttonClick(item)">
+              <template v-slot:prepend>
+                <v-icon :icon="item.icon"></v-icon>
+              </template>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-chip v-else-if="button.type == 'label'" :prepend-icon="button.icon">{{ button.label }}</v-chip>
+        <v-btn v-else @click="buttonClick(button)" :disabled="isDisabled(button)" :prepend-icon="button.icon">
+          {{ button.label }}
+        </v-btn>
+      </template>
+
+      <template v-for="filter in filters">
+        <v-select :multiple="filter.multiple" :items="filter.items" v-model="filterValues[filter.key]"
+                  :label="filter.label" density="compact" :max-width="filter.width ? filter.width : 200"
+                  return-object :clearable="!filter.multiple" min-width="135"></v-select>
+      </template>
+      <div class="toolbar__buttons__right">
+        <div v-if="pages" class="pagination">
+          <span class="page" :class="pageNr ? (pageNr == page ? 'selected' : '') : 'disabled'" v-for="pageNr in pages"
+                @click="setPage(pageNr)">
+            {{ pageNr ? pageNr : '...' }}
+          </span>
+        </div>
+        <v-select v-if="languages" :items="languages" item-title="label" @update:modelValue="changeLanguage"
+                  item-value="key" density="compact" class="language" v-model="language"/>
+        <ToolbarSearch @search="searchAction"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/css/pagination';
+
+.toolbar {
+  margin-bottom: $spaceLogoMenu;
+
+  &__buttons {
+  $self: &;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 5px;
+
+    :deep(.icon--add) {
+      top: 1px;
+    }
+
+    :deep(.icon--delete) {
+      top: 1px;
+    }
+
+    :deep(svg) {
+      width: 20px;
+      height: 20px;
+      margin-right: 5px;
+      fill: var(--color-text);
+
+      line {
+        stroke: var(--color-text);
+      }
+    }
+
+    &__right {
+      margin-left: auto;
+      display: flex;
+      gap: 5px;
+      white-space: nowrap;
+      flex-wrap: wrap;
+    }
+
+    .v-btn--disabled {
+      :deep(svg) {
+        opacity: .3;
+      }
+    }
+
+    &.wrapped {
+      #{$self}__right {
+        margin-left: 0;
+      }
+    }
+
+    .v-chip {
+      height: 36px;
+      padding: 0 16px;
+    }
+  }
+
+  .v-select {
+    height: 36px;
+
+    :deep(.v-input__details) {
+      position: absolute;
+    }
+
+    :deep(.v-input__control) {
+      height: 36px;
+    }
+
+    :deep(.v-field-label) {
+      margin-top: -1px;
+    }
+
+    :deep(.v-field--focused .v-field-label), &--selected :deep(.v-field-label) {
+      margin-top: -9px;
+      margin-left: 10px;
+      font-size: 10px;
+    }
+
+    :deep(input), :deep(.v-field__input) {
+      padding: 6px 10px;
+      margin-top: -2px;
+    }
+
+    :deep(.v-field__input) {
+      flex-flow: nowrap;
+      overflow: hidden;
+    }
+  }
+
+  .language {
+    max-width: 200px;
+  }
+}
+</style>
